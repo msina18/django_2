2d560f7f8586b6a14ace97227f536e36004f3a06
msina18 <mhmdsynasarykhany6@gmail.com>
2024-05-22 14:27:39 -0700

    django

---
+++ root/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Team , Portfolio , Skill , Service
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(Team)
+admin.site.register(Portfolio)
+admin.site.register(Skill,SkillAdmin)
+admin.site.register(Service)
